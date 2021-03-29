cace60e38429a9f428d8a17ef1207c5aa7956915
nadiabasarab <nadia.basarab@ocsbstudent.ca>
2021-03-29 16:51:53 +0000

    here we go again

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function circumferenceOfCircle () {
   var circleDiameter = parseInt(prompt("Please enter the diameter of circle:"));
-  var circumferenceOfCircle = (circleDiameter / 2) * Math.PI * 2
+  var circumferenceOfCircle () = (circleDiameter / 2) * Math.PI * 2
   document.getElementById("circumference_status").innerHTML = "The circumference of the circle is " + circumferenceOfCircle ()
 }
 function areaOfCircle () {
   var circleDiameter = parseInt(prompt("Please enter the diameter of circle:"))
-  var areaOfCircle = Math.pow(circleDiameter / 2, 2) *  Math.PI
+  var areaOfCircle () = Math.pow(circleDiameter / 2, 2) *  Math.PI
   document.getElementById("area_status").innerHTML = "The area of the circle is " + areaOfCircle ()
 }
