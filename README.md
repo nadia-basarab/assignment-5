71b2bc46b7d5082cc50f9fe75e2576304db78ef4
nadiabasarab <nadia.basarab@ocsbstudent.ca>
2021-03-29 15:58:30 +0000

    final changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # assignment-5
 Final assignment
- [![linter](https://github.com/nadia-basarab/assignment-5/workflows/linter/badge.svg)](https://github.com/marketplace/actions/super-linter) 
+ [![linter](https://github.com/nadia-basarab/assignment-5/workflows/linter/badge.svg)](https://github.com/marketplace/actions/super-linter)
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function circumferenceOfCircle(){
   document.getElementById("circumference_status").innerHTML = "The circumference of the circle is " + circumferenceOfCircle
 }
 function areaOfCircle(){
-  var circleDiameter = parseInt(prompt("Please enter the diameter of circle:")
+  var circleDiameter = parseInt(prompt("Please enter the diameter of circle:"))
   var areaOfCircle = Math.pow(circleDiameter / 2, 2) *  Math.PI
   document.getElementById("area_status").innerHTML = "The area of the circle is " + areaOfCircle
 }
