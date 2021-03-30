f0daee3cc9bd55b48badf7a2781648c4d49a56d0
nadiabasarab <nadia.basarab@ocsbstudent.ca>
2021-03-30 16:36:39 +0000

    we back

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script>
 function circumferenceOfCircle () {
   const circleDiameter = parseInt(prompt('Please enter the diameter of circle:'))
   const circumferenceOfCircle = (circleDiameter / 2) * Math.PI * 2
@@ -9,4 +8,3 @@ function areaOfCircle () {
   const areaOfCircle = Math.pow((circleDiameter / 2), 2) * Math.PI
   document.getElementById('area_status').innerHTML = 'The area of the circle is ' + areaOfCircle
 }
-</script>
