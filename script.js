2e40cf34a9b3e5c5afaffcd528daad0472c80520
nadiabasarab <nadia.basarab@ocsbstudent.ca>
2021-03-30 16:23:08 +0000

    i dont know what

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-/* exported circumferenceOfCircle */
 function circumferenceOfCircle () {
   const circleDiameter = parseInt(prompt('Please enter the diameter of circle:'))
   const circumferenceOfCircle = (circleDiameter / 2) * Math.PI * 2
   document.getElementById('circumference_status').innerHTML = 'The circumference of the circle is ' + circumferenceOfCircle
 }
-/* exported areaOfCircle */
 function areaOfCircle () {
   const circleDiameter = parseInt(prompt('Please enter the diameter of circle:'))
   const areaOfCircle = Math.pow((circleDiameter / 2), 2) * Math.PI
