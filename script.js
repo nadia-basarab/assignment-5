5c03448ba9b6ff4ad389be533991d19a2f2cc175
nadiabasarab <nadia.basarab@ocsbstudent.ca>
2021-03-30 17:12:41 +0000

    eventListener

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-element.addEventListener(function circumferenceOfCircle (){;
-element.addEventListener(function areaOfCircle (){; {
+element.addEventListener("click", circumferenceOfCircle);
+element.addEventListener("click", areaOfCircle);
+function circumferenceOfCircle () {
   const circleDiameter = parseInt(prompt('Please enter the diameter of circle:'))
   const circumferenceOfCircle = (circleDiameter / 2) * Math.PI * 2
   document.getElementById('circumference_status').innerHTML = 'The circumference of the circle is ' + circumferenceOfCircle
