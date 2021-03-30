34fab1dad3a606c2c55295dd9666109a00b07041
nadiabasarab <nadia.basarab@ocsbstudent.ca>
2021-03-30 16:49:00 +0000

    ok now

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function circumferenceOfCircle () {
+element.addEventListener(function circumferenceOfCircle ());
+element.addEventListener(function areaOfCircle ()); {
   const circleDiameter = parseInt(prompt('Please enter the diameter of circle:'))
   const circumferenceOfCircle = (circleDiameter / 2) * Math.PI * 2
   document.getElementById('circumference_status').innerHTML = 'The circumference of the circle is ' + circumferenceOfCircle
